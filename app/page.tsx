8a15abb0efc458e4cfc15c9bacf430e24c5f7bc6
Andry Pebrianto <andrypeb227@gmail.com>
2024-02-05 10:17:34 +0700

    Membuat design halaman home

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,17 @@ import React from "react";
 export default function HomePage() {
   return (
     <div className="container">
-      <h1>Home Page</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+            Save your important notes here for convenience and security.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
